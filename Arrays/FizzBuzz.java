93449dced30196cb2dd807031e1076fa5305e22c
Ayroid <ayushsk0000@gmail.com>
2022-10-12 05:44:21 +0530

    FizzBuzz

---
+++ Arrays/FizzBuzz.java
@@ -0,0 +1,31 @@
+import java.util.*;
+class FizzBuzz {
+    public List<String> fizzBuzz(int n) {
+        List<String> list=new ArrayList<String>();
+        for(int i = 1; i<=n; i++){
+            if(i%3==0 && i%5==0){
+                list.add(i-1, "FizzBuzz");
+            }
+            else if(i%3==0){
+                list.add(i-1, "Fizz");
+            }
+            else if(i%5==0){
+                list.add(i-1, "Buzz");
+            }
+            else{
+                list.add(i-1, Integer.toString(i));
+            }
+        }
+        return list;
+    }
+    public static void main(String[] args) {
+        FizzBuzz f1 = new FizzBuzz();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Number: ");
+        int num = sc.nextInt();
+        List<String> list = f1.fizzBuzz(num);
+        for(int i=0;i<list.size();i++){
+            System.out.print(list.get(i)+" ");
+        }
+    }
+}
